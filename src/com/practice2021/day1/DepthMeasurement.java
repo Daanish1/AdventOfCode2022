21e416021ece103a3c8f24ea1356d08c404b5fef
Daanish Karim <daanish.karim@gmail.com>
2022-11-28 23:59:25 +0000

    Advent of Code 2021 Day 1 and Day 2 Practice

---
+++ src/com/practice2021/day1/DepthMeasurement.java
@@ -0,0 +1,73 @@
+package com.practice2021.day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DepthMeasurement {
+
+
+    public static List<Integer> readInput(String path) {
+
+        ArrayList<Integer> inputs = new ArrayList<>();
+
+        try {
+            File file = new File(path);
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                inputs.add(Integer.parseInt(data));
+            }
+
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+
+        return inputs;
+    }
+
+    public static int howManyLarger(List<Integer> in) {
+
+        int count = 0;
+        int previous = in.get(0);
+        for (int i = 1; i < in.size(); i++) {
+            if (in.get(i) > previous) {
+                count++;
+            }
+
+            previous = in.get(i);
+        }
+
+        return count;
+
+    }
+
+    public static int howManyLargerThrees(List<Integer> in) {
+        int count = 0;
+
+        for (int i = 0; i < in.size()-3; i++) {
+            int currentSum = in.get(i) + in.get(i+1) + in.get(i+2);
+            int secondSum = currentSum + in.get(i+3) - in.get(i);
+
+            if (secondSum > currentSum) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+
+
+    public static void main(String[] args) {
+        List<Integer> in = readInput("input_1.txt");
+        System.out.println(in);
+        System.out.println(howManyLarger(in));
+
+        System.out.println(howManyLargerThrees(in));
+    }
+
+
+}
---
+++ src/com/practice2021/day2/CalculateCourse.java
@@ -0,0 +1,87 @@
+package com.practice2021.day2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CalculateCourse {
+
+
+    public static int solveSecond(String path) {
+
+        int depth = 0;
+        int horiz = 0;
+        int aim = 0;
+
+        BufferedReader objReader;
+        try {
+            String strCurrentLine;
+
+            objReader = new BufferedReader(new FileReader(path));
+
+            while ((strCurrentLine = objReader.readLine()) != null) {
+
+                String[] splitted = strCurrentLine.split(" ");
+
+                int val = Integer.parseInt(splitted[1]);
+
+                if (splitted[0].equals("forward")) {
+                    horiz += val;
+                    depth += aim * val;
+                } else if (splitted[0].equals("up")) {
+                    aim -= val;
+                } else {
+                    aim += val;
+                }
+            }
+
+
+
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
+        return depth*horiz;
+
+    }
+//    public static int solve(String path) {
+//
+//        int depth = 0;
+//        int horiz = 0;
+//
+//        BufferedReader objReader;
+//        try {
+//            String strCurrentLine;
+//
+//            objReader = new BufferedReader(new FileReader(path));
+//
+//            while ((strCurrentLine = objReader.readLine()) != null) {
+//
+//                String[] splitted = strCurrentLine.split(" ");
+//
+//                int val = Integer.parseInt(splitted[1]);
+//
+//                if (splitted[0].equals("forward")) {
+//                    horiz += val;
+//                } else if (splitted[0].equals("up")) {
+//                    depth -= val;
+//                } else {
+//                    depth += val;
+//                }
+//            }
+//
+//
+//
+//        } catch (IOException e) {
+//            System.out.println("Error");
+//        }
+//
+//        return depth*horiz;
+//
+//    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solveSecond("input_2.txt"));
+    }
+}
