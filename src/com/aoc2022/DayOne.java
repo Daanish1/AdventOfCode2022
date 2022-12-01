ecd6fcdc05ad8d9205a2ac66419f99a8c5d82c46
Daanish1 <daanish.karim@gmail.com>
2022-12-01 16:12:26 +0000

    Advent of Code Day 1: Comeplete

---
+++ src/com/aoc2022/DayOne.java
@@ -0,0 +1,96 @@
+package com.aoc2022;
+
+import com.common.usage.FileReadHelper;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DayOne {
+
+    public static int partOne(List<String> input) {
+        int maxVal = 0;
+
+        int currentCount = 0;
+        for (int i = 0; i < input.size(); i++) {
+
+            if (input.get(i).equals("")) {
+                if (currentCount > maxVal) {
+                    maxVal = currentCount;
+                }
+                currentCount = 0;
+            } else {
+                currentCount += Integer.parseInt(input.get(i));
+            }
+        }
+
+        if (maxVal > currentCount)
+            return maxVal;
+        else
+            return currentCount;
+    }
+
+    public static List<String> readFile(String path) {
+        List<String> inputs = new ArrayList<>();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            String strCurrentLine;
+            while((strCurrentLine = reader.readLine()) != null) {
+                inputs.add(strCurrentLine);
+            }
+
+            return inputs;
+
+        } catch (IOException e) {
+            System.out.println("Error");
+            return null;
+        }
+
+    }
+
+    public static int partTwo(List<String> input) {
+        List<Integer> calorieCount = new ArrayList<>();
+
+
+        int currentCount = 0;
+        for (int i = 0; i < input.size(); i++) {
+
+            if (input.get(i).equals("")) {
+                calorieCount.add(currentCount);
+                currentCount = 0;
+            } else {
+                currentCount += Integer.parseInt(input.get(i));
+            }
+        }
+
+        calorieCount.add(currentCount);
+
+        Collections.sort(calorieCount);
+
+        int lastIndex = calorieCount.size()-1;
+        int sum = calorieCount.get(lastIndex) + calorieCount.get(lastIndex-1) + calorieCount.get(lastIndex-2);
+
+        return sum;
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        List<String> input = FileReadHelper.fileToList("inputFiles/day1.txt", 1);
+        System.out.println(partOne(input));
+        System.out.println(partTwo(input));
+        
+
+
+    }
+
+
+}
